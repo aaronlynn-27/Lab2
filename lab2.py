74f20d9ffba9624e390c89da7e5f9116026ff6fc
aaronlynn <lynnthitkhant27@gmail.com>
2024-10-20 19:28:05 +0800

    modified lab2

--- lab2.py
+++ lab2.py
@@ -1,1 +1,41 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def main():
+    display_main_menu()
+
+    input_values = get_user_input()
+
+    sorted_values = sort_temp(input_values)
+
+    average_temperature = calc_average(sorted_values)
+    print(f"The average temperature is: {average_temperature:.2f}")
+
+    min_max_value = find_min_max(sorted_values)
+    print(f"The minimum and maximum temperatue: {min_max_value}")
+
+
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+def get_user_input():
+    input_number = input()
+    number_split = input_number.split(",")
+    number = [float(value) for value in number_split]
+    return number
+
+def sort_temp(temperatures):
+    temperatures.sort()
+    return temperatures
+
+def calc_average(temperatures):
+    sum_of_temperatures = sum(temperatures)
+    count = len(temperatures)
+    average_temp = sum_of_temperatures / count
+    return average_temp
+
+def find_min_max(temperatures):
+    max_temp = max(temperatures)
+    min_temp = min(temperatures)
+    return [min_temp, max_temp]
+
+if __name__ == "__main__":
+    main()
